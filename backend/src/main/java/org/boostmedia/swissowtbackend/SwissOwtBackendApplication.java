c3a2ac8cb3a1228dd440344b38c7ce71791ff82d
hamidsoleymani <hamid.soleymani@gmx.de>
2026-07-13 14:55:12 +0200

    1) Create BE, FE and database with spring boot app inside BE

---
+++ backend/src/main/java/org/boostmedia/swissowtbackend/SwissOwtBackendApplication.java
@@ -0,0 +1,13 @@
+package org.boostmedia.swissowtbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwissOwtBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwissOwtBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/org/boostmedia/swissowtbackend/SwissOwtBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.boostmedia.swissowtbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwissOwtBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
