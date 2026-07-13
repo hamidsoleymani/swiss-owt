b08318d3b44e9ab2cac03bfad45da05ea3c212d1
hamidsoleymani <hamid.soleymani@gmx.de>
2026-07-13 15:17:08 +0200

    3) Used InMemoryUserDetailsManager just to test the security configuration.

---
+++ backend/src/main/java/org/boostmedia/swissowtbackend/security/SecurityConfig.java
@@ -0,0 +1,47 @@
+package org.boostmedia.swissowtbackend.security;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+
+        http
+                .csrf(csrf -> csrf.disable())
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated())
+                .httpBasic(Customizer.withDefaults());
+
+        return http.build();
+    }
+
+    @Bean
+    InMemoryUserDetailsManager userDetailsService(PasswordEncoder encoder) {
+
+        UserDetails admin =
+                User.builder()
+                        .username("admin")
+                        .password(encoder.encode("admin"))
+                        .roles("ADMIN")
+                        .build();
+
+        return new InMemoryUserDetailsManager(admin);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
