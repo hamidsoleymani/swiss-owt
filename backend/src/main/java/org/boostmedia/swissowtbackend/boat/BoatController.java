cb435ada8856b9344637bec2789ecc12b3372403
hamidsoleymani <hamid.soleymani@gmx.de>
2026-07-13 16:16:41 +0200

    10) added crud operations

--- backend/src/main/java/org/boostmedia/swissowtbackend/boat/BoatController.java
+++ backend/src/main/java/org/boostmedia/swissowtbackend/boat/BoatController.java
@@ -3,6 +3,7 @@ package org.boostmedia.swissowtbackend.boat;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -23,5 +24,21 @@ public class BoatController {
     @PostMapping
     public BoatResponse create(@RequestBody BoatRequest request) {
         return service.create(request);
+    }
+
+    @GetMapping("/{id}")
+    public BoatResponse findById(@PathVariable Long id) {
+        return service.findById(id);
+    }
+
+    @PutMapping("/{id}")
+    public BoatResponse update(@PathVariable Long id, @RequestBody BoatRequest request) {
+        return service.update(id, request);
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void delete(@PathVariable Long id) {
+        service.delete(id);
     }
 }
--- backend/src/main/java/org/boostmedia/swissowtbackend/boat/BoatService.java
+++ backend/src/main/java/org/boostmedia/swissowtbackend/boat/BoatService.java
@@ -13,14 +13,12 @@ public class BoatService {
     private final BoatRepository repository;
     private final BoatMapper mapper;
 
-    public BoatService(BoatRepository repository,
-                       BoatMapper mapper) {
+    public BoatService(BoatRepository repository, BoatMapper mapper) {
         this.repository = repository;
         this.mapper = mapper;
     }
 
     public Page<BoatResponse> findAll(Pageable pageable) {
-
         return repository.findAll(pageable)
                 .map(mapper::toResponse);
     }
@@ -34,5 +32,31 @@ public class BoatService {
         );
 
         return mapper.toResponse(repository.save(boat));
+    }
+
+    public BoatResponse findById(Long id) {
+
+        Boat boat = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Boot not found"));
+        return mapper.toResponse(boat);
+    }
+
+    public BoatResponse update(Long id, BoatRequest request) {
+
+        Boat boat = repository.findById(id)
+                .orElseThrow(() -> new RuntimeException("Boat not found"));
+        boat.update(
+                request.name(),
+                request.description()
+        );
+        return mapper.toResponse(repository.save(boat));
+    }
+
+    public void delete(Long id) {
+
+        if (!repository.existsById(id)) {
+            throw new RuntimeException("Boat not found");
+        }
+        repository.deleteById(id);
     }
 }
