b9abdca7427bcf2cc3fe28bacac7bdf3d47e3dc7
hamidsoleymani <hamid.soleymani@gmx.de>
2026-07-13 20:18:53 +0200

    17) added login

--- frontend/boat-app/src/app/auth/login/login.ts
+++ frontend/boat-app/src/app/auth/login/login.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
+
+import { AuthService } from '../auth.service';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  standalone: true,
+  imports: [FormsModule],
   templateUrl: './login.html',
-  styleUrl: './login.scss',
+  styleUrl: './login.scss'
 })
 export class Login {
 
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
+  username = signal('');
+  password = signal('');
+
+  login(): void {
+
+    this.authService.login(
+      this.username(),
+      this.password()
+    );
+
+    this.router.navigate(['/boats']);
+  }
 }
