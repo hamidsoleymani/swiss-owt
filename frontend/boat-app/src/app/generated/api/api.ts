48cf9540182a2e3ecaad20f08e455a46a3242dc9
hamidsoleymani <hamid.soleymani@gmx.de>
2026-07-13 20:18:06 +0200

    16) deleted app.html content

--- frontend/boat-app/src/app/generated/api/api.ts
+++ frontend/boat-app/src/app/generated/api/api.ts
@@ -1,5 +1,3 @@
 export * from './boatController.service';
 import { BoatControllerService } from './boatController.service';
-export * from './helloController.service';
-import { HelloControllerService } from './helloController.service';
-export const APIS = [BoatControllerService, HelloControllerService];
+export const APIS = [BoatControllerService];
