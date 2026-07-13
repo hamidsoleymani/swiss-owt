642a3236f0bfd510df8da40750a6fff6bfa06b31
hamidsoleymani <hamid.soleymani@gmx.de>
2026-07-13 19:16:07 +0200

    15) Regenerated auth relavant classed

---
+++ frontend/boat-app/src/app/boats/boat-list/boat-list.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-boat-list',
+  imports: [],
+  templateUrl: './boat-list.html',
+  styleUrl: './boat-list.scss',
+})
+export class BoatList {
+
+}
