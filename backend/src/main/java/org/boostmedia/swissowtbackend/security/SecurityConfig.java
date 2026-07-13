4f59611f6edfc832c2d9dba257e9fa8f5230a7c9
hamidsoleymani <hamid.soleymani@gmx.de>
2026-07-13 18:47:26 +0200

    12) added openapi dependency and updated spring security

--- backend/src/main/java/org/boostmedia/swissowtbackend/security/SecurityConfig.java
+++ backend/src/main/java/org/boostmedia/swissowtbackend/security/SecurityConfig.java
@@ -24,6 +24,11 @@ public class SecurityConfig {
         http
                 .csrf(csrf -> csrf.disable())
                 .authorizeHttpRequests(auth -> auth
+                        .requestMatchers(
+                                "/v3/api-docs/**",
+                                "/swagger-ui/**",
+                                "/swagger-ui.html"
+                        ).permitAll()
                         .anyRequest().authenticated())
                 .httpBasic(Customizer.withDefaults())
                 .userDetailsService(userDetailsService);
